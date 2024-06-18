9befdef449dacdd1a19bb32f5f002f60ae73f6aa
bezumkin <bezumkin@yandex.ru>
2024-06-18 09:47:08 +0700

    Remove unneeded entities

--- core/elements/snippet.php
+++
@@ -1,7 +0,0 @@
-<?php
-/** @var \MODX\Revolution\modX $modx */
-
-if ($modx->services->has('mmxUsers')) {
-    /** @var array $scriptProperties */
-    return $modx->services->get('mmxUsers')->handleSnippet($scriptProperties);
-}
